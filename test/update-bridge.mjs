0d66c31bcb64b671fc4c14735f9e0acf2f2586d2
Manuel <msimoni+sky@gmail.com>
2024-10-30 15:08:26 +0000

    Construct UpdateBridge redeemer

--- test/update-bridge.mjs
+++ test/update-bridge.mjs
@@ -28,5 +28,25 @@ const currencySymbol = fs.readFileSync(`var/sky-minting-policy.hash`).toString()
 const publicKeyHex = process.argv[2]
 const newTopHashHex = process.argv[3]
 const sigHex = process.argv[4]
+const oldDataHashHex = '0000' // TBD: Currently ignored by contract
 
 console.log(`Updating bridge with new top hash ${newTopHashHex}\nPublic key: ${publicKeyHex}\nSignature: ${sigHex}`);
+
+// Create MultiSigPubKey
+const publicKey = {
+    alternative: 0,
+    fields: [
+	[publicKeyHex], // List of public keys in signatures
+	1 // Number of public keys that must sign
+    ]
+}
+// Create UpdateBridge redeemer
+const redeemer = {
+    alternative: 0,
+    fields: [
+	publicKey,
+	oldDataHashHex,
+	newTopHashHex,
+	sigHex
+    ]
+}
