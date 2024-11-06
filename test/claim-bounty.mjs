f34f896f4f57151ebf73015be0873e3f8513ab73
Manuel <msimoni+sky@gmail.com>
2024-11-06 13:26:48 +0000

    claim-bounty.mjs init

--- test/claim-bounty.mjs
+++ test/claim-bounty.mjs
@@ -1,1 +1,14 @@
+import { BlockfrostProvider, MeshWallet, Transaction } from '@meshsdk/core'
 
+import fs from 'node:fs'
+
+const blockfrostKey = fs.readFileSync(`var/blockfrost.api-key`).toString().trim()
+const blockchainProvider = new BlockfrostProvider(blockfrostKey)
+
+const left = process.argv[2]
+const right = process.argv[3]
+const pkHash = process.argv[4]
+
+console.log(`Left ${left}`);
+console.log(`Right ${right}`);
+console.log(`Committee hash ${pkHash}`);
