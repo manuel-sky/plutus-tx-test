d98c253326963a862fe5bfce6113f83e16bdc3cd
Manuel <msimoni+sky@gmail.com>
2024-11-06 17:33:12 +0000

    Print UTXOs at bridge

--- test/claim-bounty.mjs
+++ test/claim-bounty.mjs
@@ -1,4 +1,17 @@
-import { BlockfrostProvider, MeshWallet, Transaction } from '@meshsdk/core'
+import {
+    BlockfrostProvider,
+    MeshWallet,
+    MeshTxBuilder,
+    Transaction,
+    serializePlutusScript,
+    conStr,
+    byteString,
+    scriptAddress,
+    serializeAddressObj,
+    resolveScriptHash,
+    stringToHex
+} from '@meshsdk/core'
+import cbor from 'cbor'
 
 import fs from 'node:fs'
 
@@ -12,3 +25,42 @@ const pkHash = process.argv[4]
 console.log(`Left ${left}`);
 console.log(`Right ${right}`);
 console.log(`Committee hash ${pkHash}`);
+
+const validatorBlueprint = JSON.parse(
+  fs.readFileSync('./var/sky-bridge-validator.json')
+)
+
+const validator = {
+  code: cbor
+    .encode(
+      Buffer.from(validatorBlueprint.validators[0].compiledCode, 'hex')
+    )
+    .toString('hex'),
+  version: 'V2'
+}
+
+const validatorAddress = serializePlutusScript(validator).address
+
+const mintingPolicyBlueprint = JSON.parse(
+  fs.readFileSync('./var/sky-minting-policy.json')
+)
+
+const mintingPolicy = {
+  code: cbor
+    .encode(
+      Buffer.from(mintingPolicyBlueprint.validators[0].compiledCode, 'hex')
+    )
+    .toString('hex'),
+  version: 'V2'
+}
+
+const mintingPolicyHash = resolveScriptHash(
+  mintingPolicy.code,
+  mintingPolicy.version
+)
+
+console.log(validatorAddress)
+
+const utxos = await blockchainProvider.fetchAddressUTxOs(validatorAddress);
+
+console.log(utxos);
