f76326bd83e165b38e11ed3112309cd9fb3f3273
Manuel <msimoni+sky@gmail.com>
2024-11-07 15:03:54 +0000

    Sleep more

--- test/test-2.sh
+++ test/test-2.sh
@@ -4,7 +4,7 @@ set -eux
 # Create an offer
 
 node offer-bounty.mjs var/offerer
-sleep 5
+sleep 10
 
 # Verify that Ada has been locked at bounty
 
@@ -13,7 +13,7 @@ node verify-bounty-offered.mjs
 # Claim the offer
 
 node claim-bounty.mjs var/claimant 0000 1111 4b4f90f0670c7d8d26949bfc1b90de7e12a572094ec1cdf23fec3e1f9a4bcf71
-sleep 5
+sleep 10
 
 # Verify that Ada has been claimed
 
