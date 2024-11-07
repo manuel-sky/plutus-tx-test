8e0960cad16f72ec9b9386aef9d28288f50eba94
Manuel <msimoni+sky@gmail.com>
2024-11-07 14:59:10 +0000

    Fix order of post-setup steps, add some sleeping

--- test/post-setup.sh
+++ test/post-setup.sh
@@ -3,14 +3,15 @@ set -eux
 
 # Distribute some Ada to offerer and claimant
 node send-lovelace.mjs var/admin var/offerer 2500
+sleep 5
 node send-lovelace.mjs var/admin var/claimant 2500
+sleep 5
 # Create minting policy
 cabal run gen-minting-policy-blueprint -- "$(cat var/admin.pkh)" var/sky-minting-policy.json
-# Extract bridge validator and minting policy hashes
-cat var/sky-bridge-validator.json | jq -r '.validators[0].hash' > var/sky-bridge-validator.hash
 cat var/sky-minting-policy.json | jq -r '.validators[0].hash' > var/sky-minting-policy.hash
 # Generate Bridge Validator
 cabal run gen-validator-blueprint -- "$(cat var/sky-minting-policy.hash)" var/sky-bridge-validator.json
+cat var/sky-bridge-validator.json | jq -r '.validators[0].hash' > var/sky-bridge-validator.hash
 # Mint the bridge NFT
 node mint-nft.mjs var/admin
 # Generate Bounty validator
