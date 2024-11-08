e77552bd9a1fabec0fe3354411d36ffceca6517d
Manuel <msimoni+sky@gmail.com>
2024-11-08 12:34:48 +0000

    First rewrite of README

--- test/test-1.sh
+++ test/test-1.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 set -eux
 
-# Updates the bridge NFT and verifies that it has been updated afterwards
+# This test updates the bridge NFT two times and verifies that it has
+# been updated afterwards each time.
+
+# The single public key used to sign the update. Will be a multisig pubkey in later versions.
 
 export PUBKEY=F710662FC12F8852CBA4BAD811E770005A6E5311DA328D12FA5A46AEC707C405
+
+# Two merkle root top hashes, and their signatures.
 
 export TOPHASH1=AAA596B8A223956226D3607A37819AB7A6D588D394BB0F244C690E25A898F042
 export SIG1=886F601FCCCBD9E4699CA364D652FE9E32088B0B183C0430EFEAF89D81AD5792039F7ADF46964903A8DC2702B9E9CE6F9A318C5C76E66B2F85300DFE3BDF8C00
@@ -17,8 +22,8 @@ node update-bridge.mjs var/admin $PUBKEY $TOPHASH1 $SIG1
 
 # Verify that top hash 1 is stored in bridge
 
-for i in {1..5}; do
-  node verify-bridge.mjs $TOPHASH1 && break || { echo "Attempt $i failed, retrying in 5 seconds..."; sleep 5; }
+while true; do
+  node verify-bridge.mjs $TOPHASH1 && break || { echo "Attempt failed, retrying in 5 seconds..."; sleep 5; }
 done
 
 # Store top hash 2 in bridge
@@ -27,6 +32,8 @@ node update-bridge.mjs var/admin $PUBKEY $TOPHASH2 $SIG2
 
 # Verify that top hash 2 is stored in bridge
 
-for i in {1..5}; do
-  node verify-bridge.mjs $TOPHASH2 && break || { echo "Attempt $i failed, retrying in 5 seconds..."; sleep 5; }
+while true; do
+  node verify-bridge.mjs $TOPHASH2 && break || { echo "Attempt failed, retrying in 5 seconds..."; sleep 5; }
 done
+
+echo OK
